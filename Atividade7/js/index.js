828cf0d03e5aeee3e38320e1b4ad016e45d7b9d5
Evandro R.  Tiburcio <evandrorti@gmail.com>
2023-04-10 22:12:43 -0300

    ultimo

--- Atividade7/js/index.js
+++ Atividade7/js/index.js
@@ -1,45 +1,57 @@
-function jogar(opcao){
+var numPartidas
+const resJogo = document.getElementById("resJogo");
+
+function nPartidas(){
+    const select_numPartidas = document.getElementById("numPartidas");
+    numPartidas = select_numPartidas.value;
+    resJogo.innerHTML=""
+}
+
+function jogar(j1){
     const resultado = document.getElementById("resultado");
-    const op = document.getElementById("opcao")
-    
-    let j1;
+    let ganhador;
     let j2;
     let opcoes = new Array("Pedra","Papel","Tesoura");
-    j1 = opcao
     if (j1 > 0){
-        let jogo = Math.random();
-
-        if (jogo < 0.33){
-            j2=1;
-        }
-        else if (jogo < 0.66){
-            j2=2;
-        }
-        else{
-            j2 =3
-        }
+        j2 = Math.floor(Math.random() * 3) + 1;
 
         if (j1===j2){
             resultado.innerHTML="Empate";
+            ganhador="-";
         }
         else if (j1===1 && j2===3){
             resultado.innerHTML="Ganhou";
+            ganhador="J1";
         }
         else if (j1===2 && j2===1){
             resultado.innerHTML="Ganhou";
+            ganhador="J1";
         }
         else if (j1===3 && j2===2){
             resultado.innerHTML="Ganhou";
+            ganhador="J1";
         }
         else{
             resultado.innerHTML="Perdeu";
+            ganhador="J2";
         }
-        op.innerHTML=opcoes[j1-1]+ " x " + opcoes[j2-1];
-        console.log("Jogador 1: ",opcoes[j1-1],"Jogador 2:", opcoes[j2-1]);
+        if (numPartidas >= 2){
+            --numPartidas;
+            resJogo.innerHTML+=`<tr>
+                                    <td>${opcoes[j1-1]}</td>
+                                    <td>${opcoes[j2-1]}</td>
+                                    <td>${ganhador}</td>
+                                </tr>
+                                `
+        }
+        else {
+            nPartidas();
+            resJogo.innerHTML=`<tr>
+                                    <td>${opcoes[j1-1]}</td>
+                                    <td>${opcoes[j2-1]}</td>
+                                    <td>${ganhador}</td>
+                                </tr>
+                                `
+        }
     }
-}
-
-function opcao(txt){
-    const opcao = document.getElementById("opcao")
-    opcao.innerHTML=txt;
 }
